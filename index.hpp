ad268b38803d5db69ab40e20e1180817aad666bc
Shaxzod Bahtiyarov <shaxzodbmaster@gmail.com>
2023-08-15 17:26:56 +0500

    Create index.hpp

---
+++ index.hpp
@@ -0,0 +1,12 @@
+#ifndef LIB // !ikkimarta include qilinganda xato bo'lmasligi ucun
+#define LIB
+#include <iostream>
+#include <string>
+using namespace std;
+namespace mts {
+    string Qayt(){
+        return "Salom2";
+    }
+}
+
+#endif
