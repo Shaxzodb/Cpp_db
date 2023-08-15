30e0678c36ad6f3923be3df5dc22176c7c82fecf
Shaxzod Bahtiyarov <shaxzodbmaster@gmail.com>
2023-08-15 17:27:24 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include "index.hpp"
+using namespace std;
+//#include <fmt/format.h>
+// using namespace fmt;
+using namespace mts;
+string a = "Q";
+int main(){
+    cout << Qayt() << endl ;
+    return 0;
+}
